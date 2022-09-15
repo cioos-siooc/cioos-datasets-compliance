57dce42be7c9ca0328360986a82a5101a46c802b
Nate <nate.rosenstock@hakai.org>
2022-09-15 14:39:29 -0700

    fix typo

--- run_cioos.sh
+++ run_cioos.sh
@@ -1,5 +1,5 @@
 while read -r line; do
     host=$(echo $line | awk -F[/:] '{print $4}')
     echo $host
-    python -m erddap-compliance $line -v -f html
+    python -m erddap_compliance $line -v -f html
 done <cioos_servers
