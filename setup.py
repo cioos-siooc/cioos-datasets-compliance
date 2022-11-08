4603feca8063f56c540e530228ef79e099909ad6
Nate <nate.rosenstock@hakai.org>
2022-11-08 13:27:30 -0800

    switch to cioos branch

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name = "erddap-datasets-compliance",
     install_requires=[
-        'erddap-compliance@git+https://github.com/cioos-siooc/erddap-compliance',
+        'erddap-compliance@git+https://github.com/cioos-siooc/erddap-compliance@cioos',
     ],
 )
